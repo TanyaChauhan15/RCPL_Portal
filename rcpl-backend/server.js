c0a38da7efa9e5062b3a702dbd6f39178dae43c6
TanyaChauhan15 <tanyaaa.chauhan@gmail.com>
2026-06-04 10:40:33 +0530

    Updated backend port for deployment

--- rcpl-backend/server.js
+++ rcpl-backend/server.js
@@ -276,6 +276,8 @@ app.delete("/dashboards/:id", async (req, res) => {
   res.json({ message: "Dashboard deleted successfully" });
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
